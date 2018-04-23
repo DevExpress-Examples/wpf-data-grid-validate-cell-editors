80008fff9466b18eccaf6344cfe6f922bc89490b
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ CS/DXGrid_ValidatingEditors/Window1.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Data;
+using DevExpress.Xpf.Grid;
+
+namespace DXGrid_ValidatingEditors {
+    public partial class Window1 : Window {
+        public Window1() {
+            InitializeComponent();
+            grid.DataSource = new nwindDataSetTableAdapters.ProductsTableAdapter().GetData();
+        }
+
+        private void GridColumn_Validate(object sender, GridCellValidationEventArgs e) {
+            bool discontinued = (bool)((DataRowView)e.Row)["Discontinued"];
+            if (discontinued) {
+                double discount = 100 - (Convert.ToDouble(e.Value) * 100) / 
+                                              Convert.ToDouble(e.CellValue);
+                if (!(e.IsValid = discount > 0 && discount <= 30)) {
+                    e.ErrorType = DevExpress.XtraEditors.DXErrorProvider.ErrorType.Critical;
+                    if (discount < 0) {
+                        e.ErrorContent = string.Format("The price cannot be greater than ${0}",
+                                                    Convert.ToDouble(e.CellValue));
+                        return;
+                    }
+                    e.ErrorContent = string.Format(
+                       "The discount cannot be greater than 30% (${0}). Please correct the price.",
+                       Convert.ToDouble(e.CellValue)*0.7);
+                }
+            }
+        }
+
+        private void TableView_HiddenEditor(object sender, EditorEventArgs e) {
+            if (e.Column.FieldName != "Discontinued") return;
+            grid.View.CommitEditing();
+        }
+    }
+}
---
+++ VB/DXGrid_ValidatingEditors/Window1.xaml.vb
@@ -0,0 +1,57 @@
+﻿Imports Microsoft.VisualBasic
+Imports System
+Imports System.Collections.Generic
+Imports System.Linq
+Imports System.Text
+Imports System.Windows
+Imports System.Windows.Controls
+Imports System.Windows.Data
+Imports System.Windows.Documents
+Imports System.Windows.Input
+Imports System.Windows.Media
+Imports System.Windows.Media.Imaging
+Imports System.Windows.Navigation
+Imports System.Windows.Shapes
+Imports System.Data
+Imports DevExpress.Xpf.Grid
+
+Namespace DXGrid_ValidatingEditors
+	Partial Public Class Window1
+		Inherits Window
+		Public Sub New()
+			InitializeComponent()
+			grid.DataSource = _
+				New nwindDataSetTableAdapters.ProductsTableAdapter().GetData()
+		End Sub
+
+		Private Sub GridColumn_Validate(ByVal sender As Object, _
+			ByVal e As GridCellValidationEventArgs)
+			Dim discontinued As Boolean = _
+				CBool((CType(e.Row, DataRowView))("Discontinued"))
+			If discontinued Then
+				Dim discount As Double = 100 - (Convert.ToDouble(e.Value) * 100)  _ 
+					/ Convert.ToDouble(e.CellValue)
+				e.IsValid = discount > 0 AndAlso discount <= 30
+				If (Not e.IsValid) Then
+					e.ErrorType = DevExpress.XtraEditors.DXErrorProvider.ErrorType.Critical
+					If discount < 0 Then
+						e.ErrorContent = String.Format( _
+							"The price cannot be greater than ${0}", _
+							Convert.ToDouble(e.CellValue))
+						Return
+					End If
+					e.ErrorContent = String.Format( _
+					"The discount cannot be greater than 30% (${0}). Please correct the price.", _
+					Convert.ToDouble(e.CellValue)*0.7)
+				End If
+			End If
+		End Sub
+
+		Private Sub TableView_HiddenEditor(ByVal sender As Object, ByVal e As EditorEventArgs)
+			If e.Column.FieldName <> "Discontinued" Then
+				Return
+			End If
+			grid.View.CommitEditing()
+		End Sub
+	End Class
+End Namespace
