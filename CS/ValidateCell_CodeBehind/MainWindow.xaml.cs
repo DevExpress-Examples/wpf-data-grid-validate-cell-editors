0d53305bae59ab802cf45315b2c90185e7cfcb76
volkov.anton <anton.volkov@devexpress.com>
2021-08-16 19:18:26 +0300

    Update sample - use new Command API

---
+++ CS/ValidateCell_CodeBehind/MainWindow.xaml.cs
@@ -0,0 +1,43 @@
+﻿using DevExpress.Xpf.Grid;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows;
+
+namespace ValidateCell_CodeBehind {
+    public class Product {
+        public string ProductName { get; set; }
+        public double UnitPrice { get; set; }
+        public bool Discontinued { get; set; }
+
+        public Product(int i) {
+            ProductName = "Product" + i;
+            Discontinued = i % 5 == 0;
+        }
+    }
+    public partial class MainWindow : Window {
+        public MainWindow() {
+            InitializeComponent();
+            grid.ItemsSource = GetProductList().ToList();
+        }
+        static IEnumerable<Product> GetProductList() {
+            Random r = new Random();
+            return Enumerable.Range(0, 20).Select(i => new Product(i) { UnitPrice = r.Next(1, 50) });
+        }
+
+        void OnValidateCell(object sender, GridCellValidationEventArgs e) {
+            if(e.Column.FieldName != nameof(Product.UnitPrice) || !((Product)e.Row).Discontinued)
+                return;
+            var cellValue = (double)e.CellValue;
+            var discount = 100 - Convert.ToDouble(e.Value) / cellValue * 100;
+            if(discount > 0 && discount <= 30)
+                return;
+
+            e.IsValid = false;
+            e.ErrorType = DevExpress.XtraEditors.DXErrorProvider.ErrorType.Critical;
+            e.ErrorContent = discount < 0
+                ? $"The price cannot be greater than {cellValue}"
+                : $"The discount cannot be greater than 30% ({cellValue * 0.7}). Please correct the price.";
+        }
+    }
+}
---
+++ CS/ValidateCell_MVVM/MainViewModel.cs
@@ -0,0 +1,46 @@
+﻿using DevExpress.Mvvm;
+using DevExpress.Mvvm.DataAnnotations;
+using DevExpress.Mvvm.Xpf;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+
+namespace ValidateCell_MVVM {
+    public class Product {
+        public string ProductName { get; set; }
+        public double UnitPrice { get; set; }
+        public bool Discontinued { get; set; }
+
+        public Product(int i) {
+            ProductName = "Product" + i;
+            Discontinued = i % 5 == 0;
+        }
+    }
+    public class MainViewModel : ViewModelBase {
+        public ObservableCollection<Product> ProductList { get; }
+
+        public MainViewModel() {
+            ProductList = new ObservableCollection<Product>(GetProductList());
+        }
+        static IEnumerable<Product> GetProductList() {
+            Random r = new Random();
+            return Enumerable.Range(0, 20).Select(i => new Product(i) { UnitPrice = r.Next(1, 50) });
+        }
+
+        [Command]
+        public void ValidateCell(RowCellValidationArgs args) {
+            if(args.FieldName != nameof(Product.UnitPrice) || !((Product)args.Item).Discontinued)
+                return;
+            var cellValue = (double)args.OldValue;
+            var discount = 100 - Convert.ToDouble(args.NewValue) / cellValue * 100;
+            if(discount > 0 && discount <= 30)
+                return;
+
+            var error = discount < 0
+                ? $"The price cannot be greater than {cellValue}"
+                : $"The discount cannot be greater than 30% ({cellValue * 0.7}). Please correct the price.";
+            args.Result = new ValidationErrorInfo(error, ValidationErrorType.Critical);
+        }
+    }
+}
