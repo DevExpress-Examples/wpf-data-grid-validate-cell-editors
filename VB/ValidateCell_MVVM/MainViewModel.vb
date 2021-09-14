27031e754a989c311c3344106e32cabb297c23d7
Andrey Slabov <andrey.slabov@devexpress.com>
2021-09-14 18:00:39 +0300

    vb

---
+++ VB/ValidateCell_MVVM/MainViewModel.vb
@@ -0,0 +1,48 @@
+﻿Imports DevExpress.Mvvm
+Imports DevExpress.Mvvm.DataAnnotations
+Imports DevExpress.Mvvm.Xpf
+Imports System
+Imports System.Collections.Generic
+Imports System.Collections.ObjectModel
+Imports System.Linq
+
+Namespace ValidateCell_MVVM
+	Public Class Product
+		Public Property ProductName() As String
+		Public Property UnitPrice() As Double
+		Public Property Discontinued() As Boolean
+
+		Public Sub New(ByVal i As Integer)
+			ProductName = "Product" & i
+			Discontinued = i Mod 5 = 0
+		End Sub
+	End Class
+	Public Class MainViewModel
+		Inherits ViewModelBase
+
+		Public ReadOnly Property ProductList() As ObservableCollection(Of Product)
+
+		Public Sub New()
+			ProductList = New ObservableCollection(Of Product)(GetProductList())
+		End Sub
+		Private Shared Function GetProductList() As IEnumerable(Of Product)
+			Dim r As New Random()
+			Return Enumerable.Range(0, 20).Select(Function(i) New Product(i) With {.UnitPrice = r.Next(1, 50)})
+		End Function
+
+		<Command>
+		Public Sub ValidateCell(ByVal args As RowCellValidationArgs)
+			If args.FieldName <> NameOf(Product.UnitPrice) OrElse Not DirectCast(args.Item, Product).Discontinued Then
+				Return
+			End If
+			Dim cellValue = DirectCast(args.OldValue, Double)
+			Dim discount = 100 - Convert.ToDouble(args.NewValue) / cellValue * 100
+			If discount > 0 AndAlso discount <= 30 Then
+				Return
+			End If
+
+			Dim [error] = If(discount < 0, $"The price cannot be greater than {cellValue}", $"The discount cannot be greater than 30% ({1}). Please correct the price.")
+			args.Result = New ValidationErrorInfo([error], ValidationErrorType.Critical)
+		End Sub
+	End Class
+End Namespace
--- VB/ValidateCell_MVVM/My Project/AssemblyInfo.vb
+++ VB/ValidateCell_MVVM/My Project/AssemblyInfo.vb
@@ -4,24 +4,24 @@ Imports System.Runtime.CompilerServices
 Imports System.Runtime.InteropServices
 Imports System.Windows
 
-' General Information about an assembly is controlled through the following 
+' General Information about an assembly is controlled through the following
 ' set of attributes. Change these attribute values to modify the information
 ' associated with an assembly.
-<Assembly: AssemblyTitle("DXGrid_ValidatingEditors")>
+<Assembly: AssemblyTitle("ValidateCell_MVVM")>
 <Assembly: AssemblyDescription("")>
 <Assembly: AssemblyConfiguration("")>
 <Assembly: AssemblyCompany("")>
-<Assembly: AssemblyProduct("DXGrid_ValidatingEditors")>
-<Assembly: AssemblyCopyright("Copyright ©  2009")>
+<Assembly: AssemblyProduct("ValidateCell_MVVM")>
+<Assembly: AssemblyCopyright("Copyright ©  2021")>
 <Assembly: AssemblyTrademark("")>
 <Assembly: AssemblyCulture("")>
 
-' Setting ComVisible to false makes the types in this assembly not visible 
-' to COM components.  If you need to access a type in this assembly from 
+' Setting ComVisible to false makes the types in this assembly not visible
+' to COM components.  If you need to access a type in this assembly from
 ' COM, set the ComVisible attribute to true on that type.
 <Assembly: ComVisible(False)>
 
-'In order to begin building localizable applications, set 
+'In order to begin building localizable applications, set
 '<UICulture>CultureYouAreCodingWith</UICulture> in your .csproj file
 'inside a <PropertyGroup>.  For example, if you are using US english
 'in your source files, set the <UICulture> to en-US.  Then uncomment
@@ -32,20 +32,20 @@ Imports System.Windows
 
 
 <Assembly: ThemeInfo(ResourceDictionaryLocation.None, ResourceDictionaryLocation.SourceAssembly)> 'where the generic resource dictionary is located - where theme specific resource dictionaries are located
-    '(used if a resource is not found in the page, 
-    ' or application resource dictionaries)
-    '(used if a resource is not found in the page, 
-    ' app, or any theme specific resource dictionaries)
+									 '(used if a resource is not found in the page,
+									 ' or application resource dictionaries)
+											  '(used if a resource is not found in the page,
+											  ' app, or any theme specific resource dictionaries)
 
 
 ' Version information for an assembly consists of the following four values:
 '
 '      Major Version
-'      Minor Version 
+'      Minor Version
 '      Build Number
 '      Revision
 '
-' You can specify all the values or you can default the Build and Revision Numbers 
+' You can specify all the values or you can default the Build and Revision Numbers
 ' by using the '*' as shown below:
 ' [assembly: AssemblyVersion("1.0.*")]
 <Assembly: AssemblyVersion("1.0.0.0")>
