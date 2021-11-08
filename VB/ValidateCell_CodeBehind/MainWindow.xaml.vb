58fb98fd34b6a40fbccf8b60bbf8fc6225270216
DevExpressExampleBot <fakeEmail>
2021-11-08 19:25:27 +0300

    Source auto update [skip ci]

--- VB/ValidateCell_CodeBehind/MainWindow.xaml.vb
+++ VB/ValidateCell_CodeBehind/MainWindow.xaml.vb
@@ -1,45 +1,46 @@
-﻿Imports DevExpress.Xpf.Grid
+Imports DevExpress.Xpf.Grid
 Imports System
 Imports System.Collections.Generic
 Imports System.Linq
 Imports System.Windows
 
 Namespace ValidateCell_CodeBehind
-	Public Class Product
-		Public Property ProductName() As String
-		Public Property UnitPrice() As Double
-		Public Property Discontinued() As Boolean
 
-		Public Sub New(ByVal i As Integer)
-			ProductName = "Product" & i
-			Discontinued = i Mod 5 = 0
-		End Sub
-	End Class
-	Partial Public Class MainWindow
-		Inherits Window
+    Public Class Product
 
-		Public Sub New()
-			InitializeComponent()
-			grid.ItemsSource = GetProductList().ToList()
-		End Sub
-		Private Shared Function GetProductList() As IEnumerable(Of Product)
-			Dim r As New Random()
-			Return Enumerable.Range(0, 20).Select(Function(i) New Product(i) With {.UnitPrice = r.Next(1, 50)})
-		End Function
+        Public Property ProductName As String
 
-		Private Sub OnValidateCell(ByVal sender As Object, ByVal e As GridCellValidationEventArgs)
-			If e.Column.FieldName <> NameOf(Product.UnitPrice) OrElse Not DirectCast(e.Row, Product).Discontinued Then
-				Return
-			End If
-			Dim cellValue = DirectCast(e.CellValue, Double)
-			Dim discount = 100 - Convert.ToDouble(e.Value) / cellValue * 100
-			If discount > 0 AndAlso discount <= 30 Then
-				Return
-			End If
+        Public Property UnitPrice As Double
 
-			e.IsValid = False
-			e.ErrorType = DevExpress.XtraEditors.DXErrorProvider.ErrorType.Critical
-			e.ErrorContent = If(discount < 0, $"The price cannot be greater than {cellValue}", $"The discount cannot be greater than 30% ({1}). Please correct the price.")
-		End Sub
-	End Class
+        Public Property Discontinued As Boolean
+
+        Public Sub New(ByVal i As Integer)
+            ProductName = "Product" & i
+            Discontinued = i Mod 5 = 0
+        End Sub
+    End Class
+
+    Public Partial Class MainWindow
+        Inherits Window
+
+        Public Sub New()
+            InitializeComponent()
+            grid.ItemsSource = GetProductList().ToList()
+        End Sub
+
+        Private Shared Function GetProductList() As IEnumerable(Of Product)
+            Dim r As Random = New Random()
+            Return Enumerable.Range(0, 20).[Select](Function(i) New Product(i) With {.UnitPrice = r.Next(1, 50)})
+        End Function
+
+        Private Sub OnValidateCell(ByVal sender As Object, ByVal e As GridCellValidationEventArgs)
+            If e.Column.FieldName IsNot NameOf(Product.UnitPrice) OrElse Not CType(e.Row, Product).Discontinued Then Return
+            Dim cellValue = CDbl(e.CellValue)
+            Dim discount = 100 - Convert.ToDouble(e.Value) / cellValue * 100
+            If discount > 0 AndAlso discount <= 30 Then Return
+            e.IsValid = False
+            e.ErrorType = DevExpress.XtraEditors.DXErrorProvider.ErrorType.Critical
+            e.ErrorContent = If(discount < 0, $"The price cannot be greater than {cellValue}", $"The discount cannot be greater than 30% ({cellValue * 0.7}). Please correct the price.")
+        End Sub
+    End Class
 End Namespace
--- VB/ValidateCell_MVVM/MainViewModel.vb
+++ VB/ValidateCell_MVVM/MainViewModel.vb
@@ -1,4 +1,4 @@
-﻿Imports DevExpress.Mvvm
+Imports DevExpress.Mvvm
 Imports DevExpress.Mvvm.DataAnnotations
 Imports DevExpress.Mvvm.Xpf
 Imports System
@@ -7,42 +7,43 @@ Imports System.Collections.ObjectModel
 Imports System.Linq
 
 Namespace ValidateCell_MVVM
-	Public Class Product
-		Public Property ProductName() As String
-		Public Property UnitPrice() As Double
-		Public Property Discontinued() As Boolean
 
-		Public Sub New(ByVal i As Integer)
-			ProductName = "Product" & i
-			Discontinued = i Mod 5 = 0
-		End Sub
-	End Class
-	Public Class MainViewModel
-		Inherits ViewModelBase
+    Public Class Product
 
-		Public ReadOnly Property ProductList() As ObservableCollection(Of Product)
+        Public Property ProductName As String
 
-		Public Sub New()
-			ProductList = New ObservableCollection(Of Product)(GetProductList())
-		End Sub
-		Private Shared Function GetProductList() As IEnumerable(Of Product)
-			Dim r As New Random()
-			Return Enumerable.Range(0, 20).Select(Function(i) New Product(i) With {.UnitPrice = r.Next(1, 50)})
-		End Function
+        Public Property UnitPrice As Double
 
-		<Command>
-		Public Sub ValidateCell(ByVal args As RowCellValidationArgs)
-			If args.FieldName <> NameOf(Product.UnitPrice) OrElse Not DirectCast(args.Item, Product).Discontinued Then
-				Return
-			End If
-			Dim cellValue = DirectCast(args.OldValue, Double)
-			Dim discount = 100 - Convert.ToDouble(args.NewValue) / cellValue * 100
-			If discount > 0 AndAlso discount <= 30 Then
-				Return
-			End If
+        Public Property Discontinued As Boolean
 
-			Dim [error] = If(discount < 0, $"The price cannot be greater than {cellValue}", $"The discount cannot be greater than 30% ({1}). Please correct the price.")
-			args.Result = New ValidationErrorInfo([error], ValidationErrorType.Critical)
-		End Sub
-	End Class
+        Public Sub New(ByVal i As Integer)
+            ProductName = "Product" & i
+            Discontinued = i Mod 5 = 0
+        End Sub
+    End Class
+
+    Public Class MainViewModel
+        Inherits ViewModelBase
+
+        Public ReadOnly Property ProductList As ObservableCollection(Of Product)
+
+        Public Sub New()
+            ProductList = New ObservableCollection(Of Product)(GetProductList())
+        End Sub
+
+        Private Shared Function GetProductList() As IEnumerable(Of Product)
+            Dim r As Random = New Random()
+            Return Enumerable.Range(0, 20).[Select](Function(i) New Product(i) With {.UnitPrice = r.Next(1, 50)})
+        End Function
+
+        <Command>
+        Public Sub ValidateCell(ByVal args As RowCellValidationArgs)
+            If args.FieldName IsNot NameOf(Product.UnitPrice) OrElse Not CType(args.Item, Product).Discontinued Then Return
+            Dim cellValue = CDbl(args.OldValue)
+            Dim discount = 100 - Convert.ToDouble(args.NewValue) / cellValue * 100
+            If discount > 0 AndAlso discount <= 30 Then Return
+            Dim [error] = If(discount < 0, $"The price cannot be greater than {cellValue}", $"The discount cannot be greater than 30% ({cellValue * 0.7}). Please correct the price.")
+            args.Result = New ValidationErrorInfo([error], ValidationErrorType.Critical)
+        End Sub
+    End Class
 End Namespace
